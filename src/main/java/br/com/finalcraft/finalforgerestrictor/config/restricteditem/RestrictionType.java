cc82f113ce7661b07b624bf9b0b7a78a5d016d36
Pétrus Pradella <petrus_ant@hotmail.com>
2023-01-30 19:35:46 -0300

    Update to new PageViwer package location

--- src/main/java/br/com/finalcraft/finalforgerestrictor/config/restricteditem/RestrictionType.java
+++ src/main/java/br/com/finalcraft/finalforgerestrictor/config/restricteditem/RestrictionType.java
@@ -3,7 +3,9 @@ package br.com.finalcraft.finalforgerestrictor.config.restricteditem;
 public enum RestrictionType {
     WHITELIST("Whitelist", 0),
     RANGED("Ranged", 30),
-    AOE("AoE", 10);
+    AOE("AoE", 10),
+//    BLOCK_INTERACT("BlockInteract", 0),
+    ;
 
     private final String key;
     private final int defaultRange;
