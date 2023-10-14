2d7393d2ee16d62e6d18ae55d4fcd1648cd95e54
Pétrus Pradella <petrus_ant@hotmail.com>
2023-10-14 15:08:54 -0300

    Only start the plugin after all other plugins

--- src/main/java/br/com/finalcraft/finalforgerestrictor/FinalForgeRestrictor.java
+++ src/main/java/br/com/finalcraft/finalforgerestrictor/FinalForgeRestrictor.java
@@ -3,6 +3,7 @@ package br.com.finalcraft.finalforgerestrictor;
 import br.com.finalcraft.evernifecore.ecplugin.annotations.ECPlugin;
 import br.com.finalcraft.evernifecore.listeners.base.ECListener;
 import br.com.finalcraft.evernifecore.logger.ECLogger;
+import br.com.finalcraft.evernifecore.scheduler.FCScheduler;
 import br.com.finalcraft.finalforgerestrictor.command.CommandRegisterer;
 import br.com.finalcraft.finalforgerestrictor.config.ConfigManager;
 import br.com.finalcraft.finalforgerestrictor.integration.worldguard.WorldGuardIntegration;
@@ -29,17 +30,19 @@ public class FinalForgeRestrictor extends JavaPlugin {
 	public void onEnable() {
 		instance = this;
 
-		getLog().info("Registering Commands...");
-		CommandRegisterer.registerCommands(this);
+		FCScheduler.scheduleSyncInTicks(() -> {
+			getLog().info("Registering Commands...");
+			CommandRegisterer.registerCommands(this);
 
-		getLog().info("Loading Configuration...");
-		ConfigManager.initialize(this);
+			getLog().info("Loading Configuration...");
+			ConfigManager.initialize(this);
 
-		ECListener.register(this, PlayerListener.class);
+			ECListener.register(this, PlayerListener.class);
 
-		if (Bukkit.getPluginManager().isPluginEnabled("WorldGuard")){
-			WorldGuardIntegration.initialize();
-		}
+			if (Bukkit.getPluginManager().isPluginEnabled("WorldGuard")){
+				WorldGuardIntegration.initialize();
+			}
+		}, 1);
 	}
 
 	@ECPlugin.Reload
